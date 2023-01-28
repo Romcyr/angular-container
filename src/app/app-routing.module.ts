b0b68c7a1b0f1a59b82eee10d02b8cd6a6e73d6d
RCyro <r.cyron95@gmail.com>
2023-01-28 15:52:10 +0100

    repair router

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,17 @@
 import {RouterModule, Routes} from "@angular/router";
-import {MainContainerComponent} from "../main-container/main-container.component";
-import {ContentContainerComponent} from "../content-container/content-container.component";
-import {NavigatorComponent} from "./navigator.component";
+import {MainContainerComponent} from "./main-container/main-container.component";
+import {ContentContainerComponent} from "./content-container/content-container.component";
+import {NavigatorComponent} from "./navigator/navigator.component";
 import {NgModule} from "@angular/core";
+import {PageMainComponent} from "./page-main/page-main.component";
+import {PageProductListComponent} from "./page-product-list/page-product-list.component";
+import {PageProductFormComponent} from "./page-product-form/page-product-form.component";
 
 const routes: Routes = [
-  {path: "", redirectTo: "Main Page", pathMatch: "full"},
-  {path: "Main Page", component: MainContainerComponent},
-  {path: "Product list", component: ContentContainerComponent},
-  {path: "Product form", component: NavigatorComponent}
+  {path: "", redirectTo: "main-page", pathMatch: "full"},
+  {path: "main-page", component: PageMainComponent},
+  {path: "product-list", component: PageProductListComponent},
+  {path: "product-form", component: PageProductFormComponent}
 ]
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,19 +5,28 @@ import { AppComponent } from './app.component';
 import { NavigatorComponent } from './navigator/navigator.component';
 import { MainContainerComponent } from './main-container/main-container.component';
 import { ContentContainerComponent } from './content-container/content-container.component';
-import {RouterOutlet} from "@angular/router";
+import {RouterLink, RouterOutlet} from "@angular/router";
+import {AppRoutingModule} from "./app-routing.module";
+import { PageMainComponent } from './page-main/page-main.component';
+import { PageProductFormComponent } from './page-product-form/page-product-form.component';
+import { PageProductListComponent } from './page-product-list/page-product-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavigatorComponent,
     MainContainerComponent,
-    ContentContainerComponent
+    ContentContainerComponent,
+    PageMainComponent,
+    PageProductFormComponent,
+    PageProductListComponent
   ],
-    imports: [
-        BrowserModule,
-        RouterOutlet
-    ],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    RouterOutlet,
+    RouterLink
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
